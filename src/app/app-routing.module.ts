5b582498bfecdf081555db8f14161f8fd53ec4c0
LuisG28 <Luis.gus2869@gmail.com>
2020-06-04 00:14:47 -0500

    Version1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from '@angular/router';//Se importa a mano
+import { InicioComponent } from './component/inicio/inicio.component';
+import { LenguajesComponent } from './component/lenguajes/lenguajes.component';
+import { DetallesComponent } from './component/detalles/detalles.component';
+
+const routes: Routes=[
+  {path:'inicio', component: InicioComponent},
+  {path:'lenguajes', component: LenguajesComponent},
+  {path:'detalles', component:DetallesComponent},
+  {path:'**',pathMatch:'full', redirectTo: 'inicio'},
+ 
+];
 
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+})  
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { TarjetasComponent } from './component/tarjetas/tarjetas.component';
+import { LenguajesComponent } from './component/lenguajes/lenguajes.component';
+import { InicioComponent } from './component/inicio/inicio.component';
+import { DetallesComponent } from './component/detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TarjetasComponent,
+    LenguajesComponent,
+    InicioComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
