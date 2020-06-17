2f53b9ee279415fbd51ed466e6aeaaee5054eb42
LuisG28 <Luis.gus2869@gmail.com>
2020-06-17 18:05:01 -0500

    Version2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {SaludarComponent} from './componentes/saludar.component'
-
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormComponent } from './componentes/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludarComponent
+    SaludarComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/form/form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor(
+    private _builder: FormBuilder
+  ) { 
+    this.sigupForm=this._builder.group({
+      nombre: ['', Validators.required],//vacio es el primer valor
+      apellidos:['', Validators.required],//validators.require para requerido
+      email:['', Validators.compose([Validators.email, Validators.required])],
+      contrasenia:['', Validators.required],
+      estado:['', Validators.required],
+      sexo:['',Validators.required],
+      fecha:['', Validators.required]
+    })
+  }
+
+  sigupForm: FormGroup
+
+  ngOnInit(): void {
+  }
+
+  enviar(values){
+    console.log(values);
+  }
+
+}
